2503078cff9ec25f10dbf5db859863e8910da391
YakovMorkovkin <yakov.morkovkin@yandex.ru>
2023-04-13 22:15:53 +0300

    stats 5th rev

--- stats/dto/src/main/java/ru/practicum/statsdto/HitDtoOut.java
+++ stats/dto/src/main/java/ru/practicum/statsdto/HitDtoOut.java
@@ -1,16 +1,12 @@
 package ru.practicum.statsdto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class HitDtoOut {
-    public final String app;
-    public final String uri;
-    public final Long hits;
-
-    public HitDtoOut(String app, String uri, Long hits) {
-        this.app = app;
-        this.uri = uri;
-        this.hits = hits;
-    }
+    private String app;
+    private String uri;
+    private Long hits;
 }
