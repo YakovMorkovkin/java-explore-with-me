363dad5e6bfa0c82abefbeb328703e37b0bd0d96
YakovMorkovkin <yakov.morkovkin@yandex.ru>
2023-04-23 10:55:21 +0300

    main 4th rev

--- ewm/src/main/java/ru/practicum/Constants.java
+++ ewm/src/main/java/ru/practicum/Constants.java
@@ -4,7 +4,7 @@ import java.time.format.DateTimeFormatter;
 
 public final class Constants {
     public static final String DT_PATTERN = "yyyy-MM-dd HH:mm:ss";
-    public static final DateTimeFormatter DT_FORMATTER = DateTimeFormatter.ofPattern( "yyyy-MM-dd HH:mm:ss" );
+    public static final DateTimeFormatter DT_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     private Constants() {
     }
